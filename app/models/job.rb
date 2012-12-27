3222d605c54f75c3c59cd4e0c35d5648a2ea8af5
shonai <vic.korablin@gmail.com>
2012-12-27 11:14:29 +0400

    Added projects

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,8 @@
 class Job < ActiveRecord::Base
-  attr_accessible :description, :name, :finished
-  validates_presence_of :name
+  attr_accessible :description, :name, :finished, :project, :project_id
+  validates_presence_of :name, :project
   scope :finished, where(:finished=>true)
   scope :unfinished, where(:finished=>false)
   scope :by_time, order("updated_at DESC")
+  belongs_to :project
 end
